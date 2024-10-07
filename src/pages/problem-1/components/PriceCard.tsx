cdeb2cfd4a68dc3ade4b662269bc7493566fd0d2
sakib-xrz <sakibxrz21@gmail.com>
2024-10-07 18:28:57 +0600

    Refactor PriceCard component to accept dynamic plan data and features; update styles and add button interactions

--- src/pages/problem-1/components/PriceCard.tsx
+++ src/pages/problem-1/components/PriceCard.tsx
@@ -1,15 +1,95 @@
 import { Info } from "lucide-react";
 
-export default function PriceCard() {
+interface PlanDetails {
+  price: string;
+  price_postfix: string;
+  plan_type: string;
+  plan_info: string;
+  dd_text: string;
+  btn_text: string;
+  price_id: string;
+}
+
+interface PlanType {
+  name: string;
+  price: string;
+  title: string;
+  text: string;
+  details: {
+    "1_year": PlanDetails;
+    "2_year": PlanDetails;
+  };
+}
+
+interface FeaturesType {
+  is_pro: string;
+  feature_title: string;
+  feature_desc: string;
+}
+
+const colors: Record<
+  string,
+  { primary: string; secondary: string; primaryHover: string }
+> = {
+  Free: {
+    primary: "#4cb3fd",
+    secondary: "#e5f2ff",
+    primaryHover: "#1884d1",
+  },
+  Basic: {
+    primary: "#ffb72c",
+    secondary: "#fff0d9",
+    primaryHover: "#d69312",
+  },
+  Pro: {
+    primary: "#68cb9b",
+    secondary: "#d8fdf0",
+    primaryHover: "#40a373",
+  },
+  Growth: {
+    primary: "#b78deb",
+    secondary: "#f7f5fb",
+    primaryHover: "#6e41a6",
+  },
+};
+
+export default function PriceCard({
+  plan,
+  features,
+}: {
+  plan: PlanType;
+  features: FeaturesType[];
+}) {
+  const { name, price, title } = plan;
+  const planColors = colors[name] || colors["Free"];
+
+  const filteredFeatures = features.filter((feature) =>
+    name === "Free" ? feature.is_pro === "0" : feature.is_pro === "1"
+  );
+
   return (
-    <div className="p-4 bg-white border border-[#eaeff2] rounded-lg py-6 px-5 border-t-8 border-t-[#ffb72c] relative transition-all delay-[.25s]">
-      <span className="absolute rounded-[3px] text-white bg-[#68cb9b] font-medium text-xs right-[6px] top-[6px] py-[6.5px] px-2">
-        Most Popular
-      </span>
-      <p className="text-lg leading-4 text-[#49687e]">Free</p>
+    <div
+      className="p-4 bg-white border border-[#eaeff2] rounded-lg py-6 px-5 relative transition-all delay-[.25s] flex flex-col"
+      style={{
+        borderTopColor: planColors.primary,
+        borderTopWidth: "8px",
+      }}
+    >
+      {name === "Pro" && (
+        <span className="absolute rounded-[3px] text-white bg-[#68cb9b] font-medium text-xs right-[6px] top-[6px] py-[6.5px] px-2">
+          Most Popular
+        </span>
+      )}
+
+      <p className="text-lg leading-4 text-[#49687e]">{name}</p>
 
       <div className="py-[10px] relative flex items-baseline mb-1 gap-[10px]">
-        <span className="font-semibold text-[32px] text-[#ffb72c]">$12.5</span>
+        <span
+          className="font-semibold text-[32px]"
+          style={{ color: planColors.primary }}
+        >
+          {price}
+        </span>
         <div>
           <span className="absolute top-[16px] text-xs line-through text-[#ff424d]">
             $15/Month
@@ -18,28 +98,56 @@ export default function PriceCard() {
         </div>
       </div>
 
-      <div className="flex gap-[5px] items-center bg-[#fff0d9] text-[#ffb72c] rounded-[32px] text-[12.5px] py-[5px] px-[15px]">
-        <span className="pt__plan-info__visitors__title">
-          Up to <strong>10,000</strong> visitors/month
-        </span>
-        <span>
-          <Info className="size-4" />
-        </span>
+      <div
+        className="flex gap-[5px] items-center rounded-[32px] text-[12.5px] py-[5px] px-[15px]"
+        style={{
+          color: planColors.primary,
+          backgroundColor: planColors.secondary,
+        }}
+      >
+        <span
+          className="prose"
+          dangerouslySetInnerHTML={{ __html: title }}
+        ></span>
+        <Info className="size-4" />
       </div>
 
       <div className="pt-[18px]">
-        <h2 className="font-semibold text-[#49687e] py-2">
-          Everything in free plus:
-        </h2>
+        {name !== "Free" ? (
+          <h2 className="font-semibold text-[#49687e] py-2">
+            Everything in Free plus:
+          </h2>
+        ) : (
+          <h2 className="font-semibold text-[#49687e] py-2">Free includes:</h2>
+        )}
       </div>
 
-      <ul className="list-none mb-3">
-        <li className="py-[10px]">
-          <div>
-            <span className="text-[#49687e]">Traffic source targeting</span>
-          </div>
-        </li>
+      <ul className="list-none mb-4">
+        {filteredFeatures.map((feature, index) => (
+          <li className="py-[5px]" key={index}>
+            <div>
+              <span className="text-[#49687e] text-sm">
+                {feature.feature_title}
+              </span>
+            </div>
+          </li>
+        ))}
       </ul>
+
+      <button
+        className="rounded-[8px] text-center text-white w-full py-2 mt-auto transition-all"
+        style={{
+          backgroundColor: planColors.primary,
+        }}
+        onMouseEnter={(e) => {
+          e.currentTarget.style.backgroundColor = planColors.primaryHover;
+        }}
+        onMouseLeave={(e) => {
+          e.currentTarget.style.backgroundColor = planColors.primary;
+        }}
+      >
+        Select Plan
+      </button>
     </div>
   );
 }
--- src/pages/problem-1/index.tsx
+++ src/pages/problem-1/index.tsx
@@ -1,14 +1,50 @@
 import Container from "../../components/Container";
 import PriceCard from "./components/PriceCard";
+import jsonData from "../../../data/pricing.json";
+import { useState } from "react";
+import { cn } from "../../lib/utils";
 
 export default function Problem1() {
+  const { plans, plansInfo, features } = jsonData;
+  const [billType, setBillType] = useState(plansInfo["1_year"].title);
+
+  const uniquePlans = plans.filter(
+    (plan, index, self) => self.findIndex((t) => t.name === plan.name) === index
+  );
+
+  const renderButton = (type: keyof typeof plansInfo) => (
+    <button
+      onClick={() => setBillType(plansInfo[type].title)}
+      className={cn(
+        "border-b-2 border-b-transparent bg-transparent cursor-pointer py-1 text-[#49687e]",
+        billType === plansInfo[type].title
+          ? "border-b-2 border-b-[#b78deb] font-medium text-[#b78deb]"
+          : ""
+      )}
+    >
+      {plansInfo[type].title}
+    </button>
+  );
+
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-4">
-        <PriceCard />
-        <PriceCard />
-        <PriceCard />
-        <PriceCard />
+      <div className="flex justify-center items-center">
+        <div className="border-r border-r-[#c6d7e3] pr-[17px] mr-[17px]">
+          {renderButton("1_year")}
+        </div>
+        {renderButton("2_year")}
+        <span
+          onClick={() => setBillType(plansInfo["2_year"].title)}
+          className="flex items-center bg-[rgba(183,141,235,0.15)] rounded-[40px] cursor-pointer ml-[10px] px-3 py-[5px] text-[#49687e]"
+        >
+          {plansInfo["2_year"].discount}
+        </span>
+      </div>
+
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-[30px]">
+        {uniquePlans.map((plan, index) => (
+          <PriceCard key={index} plan={plan} features={features} />
+        ))}
       </div>
     </Container>
   );
