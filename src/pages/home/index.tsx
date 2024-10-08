dcdbe0614710f266e0ef47e65a15b7509892f619
sakib-xrz <sakibxrz21@gmail.com>
2024-10-08 20:32:53 +0600

    Remove Problem 1, Problem 2, and Problem 3 components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,60 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { setPlans, setBillType } from "../../redux/features/plans/planSlice";
+import { useAppDispatch, useAppSelector } from "../../redux/hook";
+import Container from "../../components/Container";
+import PriceCard from "./components/PriceCard";
+import jsonData from "../../../data/pricing.json";
+import { cn } from "../../lib/utils";
+import { RootState } from "../../redux/store";
 
 export default function Home() {
+  const dispatch = useAppDispatch();
+  const { plans, billType } = useAppSelector((state: RootState) => state.plans);
+  const { plansInfo, features } = jsonData;
+
+  useEffect(() => {
+    dispatch(setPlans(jsonData.plans));
+  }, [dispatch]);
+
+  const renderButton = (type: keyof typeof plansInfo) => (
+    <button
+      onClick={() => dispatch(setBillType(plansInfo[type].title))}
+      className={cn(
+        "border-b-2 border-b-transparent bg-transparent cursor-pointer py-1 text-[#49687e] max-sm:text-sm",
+        billType === plansInfo[type].title
+          ? "border-b-2 border-b-[#b78deb] font-medium text-[#b78deb]"
+          : ""
+      )}
+    >
+      {plansInfo[type].title}
+    </button>
+  );
+
   return (
-    <div>
-      <Link to="/problem-1">Problem 1</Link>
-    </div>
+    <Container className="my-14">
+      <div className="flex justify-center items-center">
+        <div className="border-r border-r-[#c6d7e3] max-sm:pr-2 max-sm:mr-2 pr-[17px] mr-[17px]">
+          {renderButton("1_year")}
+        </div>
+        {renderButton("2_year")}
+        <span
+          onClick={() => dispatch(setBillType(plansInfo["2_year"].title))}
+          className="flex items-center bg-[rgba(183,141,235,0.15)] rounded-[40px] cursor-pointer ml-[10px] px-3 py-[5px] text-[#49687e] max-sm:text-sm"
+        >
+          {plansInfo["2_year"].discount}
+        </span>
+      </div>
+
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-[30px]">
+        {plans.map((plan, index) => (
+          <PriceCard
+            key={index}
+            plan={plan}
+            features={features}
+            billType={billType}
+          />
+        ))}
+      </div>
+    </Container>
   );
 }
--- src/router/route.tsx
+++ src/router/route.tsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home";
-import Problem1 from "../pages/problem-1";
-import Problem2 from "../pages/problem-2";
-import Problem3 from "../pages/problem-3";
 
 const router = createBrowserRouter([
   {
@@ -14,18 +11,6 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
-      },
-      {
-        path: "/problem-1",
-        element: <Problem1 />,
-      },
-      {
-        path: "/problem-2",
-        element: <Problem2 />,
-      },
-      {
-        path: "/problem-3",
-        element: <Problem3 />,
       },
     ],
   },
